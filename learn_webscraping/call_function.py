449c9a069d31e7d9c8bb01684a1ea6e69bef5a9b
Kunhee Lee <kunheeuk@naver.com>
2020-10-12 14:42:58 +0900

    mongoconnect

--- learn_webscraping/call_function.py
+++ learn_webscraping/call_function.py
@@ -1,4 +1,4 @@
-import function # import(불러오기)를 할 수 있는 것은 단 두 개 (함수 또는 클래스)
+#import function # import(불러오기)를 할 수 있는 것은 단 두 개 (함수 또는 클래스)
                 # 그러나 파일자체를 불러오면 잠정적으로 클래스로 인식하여 파일 내에 있는 모든 함수 사용 가능
 # print("="*100)
 # function.sum()
@@ -11,11 +11,19 @@ import time
 #     time.sleep(2)
 #     break
 
+from pymongo import MongoClient
 import schedule
-def job():
-    print("I'm working...")
 
-schedule.every(5).seconds.do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+db_url = 'mongodb://192.168.219.124:27017/' # ip는 계속 바뀐다. 그러므로 항상 ip주소를 재확인하자
+with MongoClient(db_url) as client:
+    sampledb = client['schedule']
+    #dic = {'아몰랑':'나도몰랑'}
+    #sampledb.samplecollection.insert_one(dic)
+
+    def job():
+        print("I'm working...")
+    schedule.every(30).minutes.do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
